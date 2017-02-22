781292a3dd6481e6dee7f47cdb11bfa6bd7906fe
wiirlock <wiirlock@gmail.com>
2017-02-22 10:31:08 +0300

    fix traffic array generate

--- handlers/generators.go
+++ handlers/generators.go
@@ -13,25 +13,14 @@ func BuildTrafficArray(deviceID bson.ObjectId) map[string][][]string {
 	var input [][]string
 	var output [][]string
 
-	firstInput := "0"
-	firstOutput := "0"
-
 	for _, stat := range devstatusData {
 		if stat.CheckType == "bytes-in" {
-			if firstInput == "0" {
-				firstInput = stat.Value
-			}
-			tmp := []string{strconv.FormatInt(stat.Time.Unix(), 10), BitPerSecond(firstInput, stat.Value)}
+			tmp := []string{strconv.FormatInt(stat.Time.Unix(), 10), stat.Value}
 			input = append(input, tmp)
-			firstInput = stat.Value
 		}
 		if stat.CheckType == "bytes-out" {
-			if firstOutput == "0" {
-				firstOutput = stat.Value
-			}
-			tmp := []string{strconv.FormatInt(stat.Time.Unix(), 10), BitPerSecond(firstOutput, stat.Value)}
+			tmp := []string{strconv.FormatInt(stat.Time.Unix(), 10), stat.Value}
 			output = append(output, tmp)
-			firstOutput = stat.Value
 		}
 	}
 
@@ -40,14 +29,4 @@ func BuildTrafficArray(deviceID bson.ObjectId) map[string][][]string {
 		"output": output,
 	}
 	return result
-}
-
-func BitPerSecond(first string, next string) string {
-	num1, _ := strconv.ParseInt(first, 10, 64)
-	num2, _ := strconv.ParseInt(next, 10, 64)
-	result := (num2 - num1)
-	if result < 0 {
-		result = 0
-	}
-	return strconv.FormatInt(result, 10)
 }
