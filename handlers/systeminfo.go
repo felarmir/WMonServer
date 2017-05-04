29b5087590b9db1672f7f36876548dc9df3a4887
wiirlock <wiirlock@gmail.com>
2017-05-04 11:12:21 +0300

    after gofmt

--- handlers/systeminfo.go
+++ handlers/systeminfo.go
@@ -2,34 +2,40 @@ package handlers
 
 import (
 	"bytes"
-    "log"
-    "os/exec"
-    "strconv"
-    "strings"
+	"log"
+	"os/exec"
+	"strconv"
+	"strings"
 )
 
 func CPU_load() float64 {
 	var load float64
 	cmd := exec.Command("ps", "aux")
-    var out bytes.Buffer
-    cmd.Stdout = &out
-    err := cmd.Run()
-    if err != nil { log.Fatal(err) }
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    for {
-        line, err := out.ReadString('\n')
-        if err!=nil { break }
-        tokens := strings.Split(line, " ")
-        ft := make([]string, 0)
-        for _, t := range(tokens) {
-            if t!="" && t!="\t" {
-                ft = append(ft, t)
-            }
-        }
-        cpu, err := strconv.ParseFloat(strings.Replace(ft[2], ",", ".", -1), 64)
-        if err != nil { continue }
-        load +=cpu
-    }
-    
-    return load
+	for {
+		line, err := out.ReadString('\n')
+		if err != nil {
+			break
+		}
+		tokens := strings.Split(line, " ")
+		ft := make([]string, 0)
+		for _, t := range tokens {
+			if t != "" && t != "\t" {
+				ft = append(ft, t)
+			}
+		}
+		cpu, err := strconv.ParseFloat(strings.Replace(ft[2], ",", ".", -1), 64)
+		if err != nil {
+			continue
+		}
+		load += cpu
+	}
+
+	return load
 }
